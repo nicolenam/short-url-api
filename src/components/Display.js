ff63f8356b296159351f580621bbf86922ff33ff
nicole-nam <eunjungnam@gmail.com>
2023-06-14 11:00:44 -0400

    Added reset on form submit and added link object into the array to get rid of unique key warning

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,7 +1,18 @@
-const Display = () =>{
+const Display = ({links}) =>{
+console.log(links)
     return(
         <div>
             display shortened url
+            {
+               links.map((link) =>{
+                    console.log(link);
+                    return(
+                        <div key={link.id}>
+                            <p>{link.link}</p>
+                        </div>
+                    )
+               })
+            }
         </div>
     )
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,36 +1,56 @@
 import axios from "axios";
-import Display from './components/Display';
-import { useEffect, useState } from "react";
+import Display from './Display';
+import { useState } from "react";
 
 const Form = () =>{
 
     const [links, setLinks] = useState([]);
+    const [textInput, setTextInput] = useState("");
 
-    useEffect(()=>{
+
+    const getData = () =>{
 
         axios({
             url: "https://api.shrtco.de/v2/shorten?",
             params: {
-                url: "https://www.google.com/search?q=entry+front+end+developer+jobs&rlz=1C5CHFA_enCA932CA932&oq=entry+front+end+developer&aqs=chrome.1.69i57j0i512l3j0i22i30l2j69i60l2.2183j0j4&sourceid=chrome&ie=UTF-8&ibp=htl;jobs&sa=X&ved=2ahUKEwjBqa-q8sL_AhUjlYkEHf_eBTUQkd0GegQIJRAB#fpstate=tldetail&htivrt=jobs&htiq=entry+front+end+developer+jobs&htidocid=RZrVC2A64B4AAAAAAAAAAA%3D%3D&sxsrf=APwXEdfg8AaZyWn1JG-GLHm5kwo3pXdUXg:1686750246989"
+                url: textInput
             }
         }).
         then((res)=>{
-            console.log(res.data.result.short_link);
+            console.log(res.data.result.code);
+
+            const linksObject = {
+                id: res.data.result.code,
+                link: res.data.result.short_link
+            }
+            setLinks((prev)=>[...prev, linksObject]);
         }).
         catch(error =>{
-            console.log(error)
+            console.log(error);
         })
+    }
 
-    },[]);
+
+    const handleChange = (e) =>{
+
+        setTextInput(e.target.value)
+    }
+
+    const handleSubmit = (e) =>{
+
+        e.preventDefault();
+        e.target.reset();
+        getData();
+    }
 
     return(
         <>
-            <form className="form">
+            <form className="form" onSubmit={handleSubmit}>
                 <label className="sr-only">Url</label>
-                <input type="text"></input>
+                <input type="text" onChange={handleChange} placeholder="Shorten a link here..."></input>
                 <button>Shorten It!</button>
             </form>
-            <Display />
+            <Display links={links}/>
         </>
     )
 }
